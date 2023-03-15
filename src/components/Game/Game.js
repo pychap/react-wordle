d81c986a0641a61e74d523fd61b994b165211cb2
Peter Chapman <pchapman@ptc.com>
2023-03-15 11:53:04 -0700

    Change naming from PreviousGessses>GuessResults

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -3,7 +3,7 @@ import React from "react";
 import { sample } from "../../utils";
 import { WORDS } from "../../data";
 import GuessInput from "../GuessInput/GuessInput";
-import PreviousGuesses from "../PreviousGuesses/PreviousGuesses";
+import GuessResults from "../GuessResults/GuessResults";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -22,7 +22,7 @@ function Game() {
 };
   return (
     <>
-    <PreviousGuesses guesses={guesses} />
+    <GuessResults guesses={guesses} />
     <GuessInput handleSubmitGuess={handleSubmitGuess} />
     </>
 )}
--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function PreviousGuesses({ guesses }) {
+function GuessResults({ guesses }) {
   
   return (
     <div className="guess-results">
@@ -11,4 +11,4 @@ function PreviousGuesses({ guesses }) {
   );
 }
 
-export default PreviousGuesses;
+export default GuessResults;
