50cabe8ad024912ce7b40db74b125f623937f514
Peter Chapman <pychapman22@gmail.com>
2023-03-16 15:57:05 -0700

    Exercise 4 (#6)
    * Change naming from PreviousGessses>GuessResults
    
    * Exercise 3, doesn't work.
    
    * Fix: pull value from object, test if undefined.
    
    * Exercise 4, compare answer to guess, set styles
    
    ---------
    
    Co-authored-by: Peter Chapman <pchapman@ptc.com>

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -22,7 +22,7 @@ function Game() {
 };
   return (
     <>
-    <GuessResults guesses={guesses} />
+    <GuessResults guesses={guesses} answer={answer} />
     <GuessInput handleSubmitGuess={handleSubmitGuess} />
     </>
 )}
