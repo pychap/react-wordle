8b2a78ae6cedfa45271cc8a81998a8442887d67b
Peter Chapman <pchapman@ptc.com>
2023-03-16 15:39:29 -0700

    Exercise 4, compare answer to guess, set styles

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React from "react";
 
-import { range } from '../../utils';
+import { range } from "../../utils";
+import { checkGuess } from "../../game-helpers";
 
-function Guess({ value }) {
+function Cell({ letter, status }) {
+  const className = status ? `cell ${status}` : "cell";
+  return <span className={className}>{letter}</span>;
+}
+
+function Guess({ value, answer }) {
   const word = value?.value;
-  console.log('The word is: ', word)
+  const result = checkGuess(word, answer);
+
   return (
     <p className="guess">
       {range(5).map((num) => (
-        <span key={num} className="cell">
-          {word ? word[num] : undefined}
-        </span>
+        <Cell
+          key={num}
+          letter={result ? result[num].letter : undefined}
+          status={result ? result[num].status : undefined}
+        />
       ))}
     </p>
   );
