511d3723c35e55421d8a91f714e9f8db75ce7794
Peter Chapman <pychapman22@gmail.com>
2023-03-17 15:01:57 -0700

    Final exercise (#7)
    * Change naming from PreviousGessses>GuessResults
    
    * Exercise 3, doesn't work.
    
    * Fix: pull value from object, test if undefined.
    
    * Exercise 4, compare answer to guess, set styles
    
    * Banner to components, won/lost and trys logic.
    
    ---------
    
    Co-authored-by: Peter Chapman <pchapman@ptc.com>

---
+++ src/components/WonBanner/WonBanner.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Banner from "../Banner/Banner";
+
+function WonBanner({ numOfGuesses}) {
+  return (
+    <Banner status="happy">
+        <p>
+          <strong>Congratulations!</strong> Got it in
+          <strong>
+            &nbsp;
+            {numOfGuesses === 1 
+            ? "1 guess" 
+            : `${numOfGuesses} guesses`
+            }
+          </strong>
+          .
+        </p>
+    </Banner>
+  );
+}
+
+export default WonBanner;
